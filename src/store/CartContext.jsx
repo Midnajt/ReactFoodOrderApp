d482d903467c7ee648739ad4b2c3d301195afc79
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-03-08 15:03:25 +0100

    CartContext finished and implemented

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -28,16 +28,47 @@ function cartReducer(state, action) {
   }
 
   if (action.type === "REMOVE_ITEM") {
-    // ... remove an item from the state
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id); // sprawdzi czy taki item jest juz w state
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items]; // kopia obecnego stanu koszyka
+
+    if (existingCartItem.quantity === 1) {
+      // jezeli w koszyku jest jeden taki element
+      updatedItems.splice(existingCartItemIndex, 1); //usuniecie 1 elementu z wskazanego indeksu
+    } else {
+      //pomniejszamy o 1
+      //tworzymy nowy element opierajac sie na starym i zmniejszajac ilosc jego ilosc o 1
+      const updatedItem = { ...existingCartItem, quantity: existingCartItem.quantity - 1 }; // aktualizowanie stanu o ilosc pomniejszona o 1
+      updatedItems[existingCartItemIndex] = updatedItem; //aktualizujemy element
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  // z provier korzysta <App/> jako rodzic to pozostalych komponentow
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext.Provider>{children}</CartContext.Provider>;
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
 }
 
-export default CartContext;
+export default CartContext; // z tego defaultowego eksportu korzystajac komponenty ktore zaciagaja context np mealitem. const CartContext zostal zadeklarowany na samej gorze
